56b431f364c876fee4f5ede5790ef41f2885b8cb
serprime <philipp.a.steinwender@gmail.com>
2011-05-15 22:30:40 +0200

    hab den code auf MVC umbaut

--- db_connect.php
+++ db_connect.php
@@ -2,14 +2,7 @@
 
 	$DatabaseHost = 'localhost';
 	$DatabaseUser = 'root';
-	$DatabasePassword = '';
+	$DatabasePassword = 'df';
 	$Database = 'finance';
 
-					
-$con = mysql_connect($DatabaseHost,$DatabaseUser,$DatabasePassword);
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
-mysql_select_db($Database) or die(mysql_error());
 ?>

018d1cb9a9fafa8dc5b89b17052881f9abff259f
serprime <philipp.a.steinwender@gmail.com>
2011-06-11 22:23:04 +0200

    neuer shit, yo

--- main.php
+++ main.php
@@ -5,14 +5,20 @@
 <div class="content-header">
 </div> 
 
-<form method="post" action="">
+<div class="logout">
+    <a href="?logout">logout</a>
+</div>
+
+<form method="post" action="?">
   <input type="text" name="v_value" /><br />
   <textarea name="v_text"></textarea><br />
   <input type="submit" value="hinzu" />
 </form>
+
 <div id="diff">
   <?php echo $this->diffUsername?> <?php echo $this->diffAmount?>
 </div>
+
 <div id="vielieb">
   <h2>Vielieb</h2>
   <?php echo $this->pcol?>

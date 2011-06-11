7352cc224cd5eb9b5c29f0d1c18f3ff0c58cc7ee
serprime <philipp.a.steinwender@gmail.com>
2011-06-11 23:11:48 +0200

    irgendwas

--- layout.php
+++ layout.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
-<head>
-  <script type="text/javascript" src="script.js"></script>
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="styles.css">
-</head>
-<body>
+<html>
+  <head>
+    <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+  </head>
+  <body>
 
-    <div class="flash">
-        <?php foreach($this->flash as $msg):?>
-            <?php echo $msg?>
-        <?php endforeach?>
-    </div>
+      <div class="flash">
+          <?php foreach($this->flash as $msg):?>
+              <?php echo $msg?>
+          <?php endforeach?>
+      </div>
 
-    <div id="wrapper">
-        <?php echo $this->content?>
-    </div>
-</body>
+      <div id="wrapper">
+          <?php echo $this->content?>
+      </div>
+  </body>
 </html>
--- main.php
+++ main.php
@@ -1,31 +1,33 @@
-<div id="header">
-  <h3>Wooohiii</h3>
+<div class="main">
+  <div id="header">
+    <h3>Wooohiii</h3>
+  </div>
+
+  <div class="content-header">
+  </div> 
+
+  <div class="logout">
+      <a href="?logout">logout</a>
+  </div>
+
+  <form method="post" action="?">
+    <input type="text" name="v_value" /><br />
+    <textarea name="v_text"></textarea><br />
+    <input type="submit" value="hinzu" />
+  </form>
+
+  <div id="diff">
+    <?php echo $this->diffUsername?> <?php echo $this->diffAmount?>
+  </div>
+
+  <div id="vielieb">
+    <h2>Vielieb</h2>
+    <?php echo $this->pcol?>
+  </div>
+
+  <div id="sarah">
+    <h2>Sarah</h2>
+    <?php echo $this->scol?>
+  </div>
+  <div class="clear"></div>
 </div>
-
-<div class="content-header">
-</div> 
-
-<div class="logout">
-    <a href="?logout">logout</a>
-</div>
-
-<form method="post" action="?">
-  <input type="text" name="v_value" /><br />
-  <textarea name="v_text"></textarea><br />
-  <input type="submit" value="hinzu" />
-</form>
-
-<div id="diff">
-  <?php echo $this->diffUsername?> <?php echo $this->diffAmount?>
-</div>
-
-<div id="vielieb">
-  <h2>Vielieb</h2>
-  <?php echo $this->pcol?>
-</div>
-
-<div id="sarah">
-  <h2>Sarah</h2>
-  <?php echo $this->scol?>
-</div>
-
